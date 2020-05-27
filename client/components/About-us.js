8696bc90a675f516be986f055346b56ed18231f4
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2020-05-27 20:34:16 +0000

    fix for the use of a deprecated directive in nginx

--- client/components/About-us.js
+++ client/components/About-us.js
@@ -17,4 +17,6 @@ class AboutUs extends React.Component {
             </Card.Title>
             <img
               className="d-block w-100"
-              src="https://www.roastycoffee.com/wp-content/uploads/blake-wisz-549106-unsplash
+              src="https://www.roastycoffee.com/wp-content/uploads/blake-wisz-549106-unsplash-e1531935387231.jpg"
+              alt="About-us-coffee"
+          
