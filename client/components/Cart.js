3ad5f0c7b919120a57ef513b695bb32c84b4d145
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2020-06-04 07:58:51 +0000

    Fix a comment.

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -3,4 +3,7 @@ import Button from 'react-bootstrap/Button'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import CartItem from './CartItem'
-import {fetchCartItems} fr
+import {fetchCartItems} from '../store/cart'
+
+class Cart extends React.Component {
+  constructor(p
