5f3b298a691c05ccc8839e50de1da5d7e02b8aa6
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2020-07-03 21:50:15 +0000

    aws

--- client/components/CartItem.js
+++ client/components/CartItem.js
@@ -5,4 +5,5 @@ const CartItem = props => {
   const {imgUrl, quantity, price, weight, name} = props.item
   //const qty = props.item.OrdersProducts.quantity
   return (
-    
+    <Card style={{width: '600px'}} className="cart">
+      <Card.Bod
