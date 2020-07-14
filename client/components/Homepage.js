1d6e8baff78a270caffa61c55946315de47d99be
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2020-07-14 15:59:42 +0000

    Delete script.js

--- client/components/Homepage.js
+++ client/components/Homepage.js
@@ -15,3 +15,32 @@ export const HomePage = props => {
           />
           <Carousel.Caption>
             <h1>Welcome to Expresso.js</h1>
+            <h3>...made by techies, for techies.</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://cdn1.medicalnewstoday.com/content/images/articles/323/323594/white-cup-with-black-coffee.jpg"
+            alt="Coffee Slide 2"
+          />
+
+          <Carousel.Caption>
+            <h1>Espresso yourself</h1>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.pexels.com/photos/982612/pexels-photo-982612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            alt="Coffee Slide 3"
+          />
+
+          <Carousel.Caption>
+            <h1>The best coffee by us, for you, every day.</h1>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  )
+}
