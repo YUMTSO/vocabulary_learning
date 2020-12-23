67f9ef0308c4d00bb8f336579cd2daec85fbdc94
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2020-12-23 18:33:54 +0000

    Sloader update on _data/MLDotNet.json

--- client/components/SingleCoffeePage.js
+++ client/components/SingleCoffeePage.js
@@ -3,4 +3,7 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Button from 'react-bootstrap/Button'
-import {getSingleCoffee} from '../store
+import {getSingleCoffee} from '../store'
+import {addToCart} from '../store/cart'
+
+export class SingleCoffe
