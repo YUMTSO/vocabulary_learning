6831084f494cd95edc87c0a93419bbb50679d5ae
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2020-12-23 18:28:16 +0000

    - update readme

--- client/components/SingleCoffeePage.js
+++ client/components/SingleCoffeePage.js
@@ -2,4 +2,5 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import Jumbotron from 'react-bootstrap/Jumbotron'
-import 
+import Button from 'react-bootstrap/Button'
+import {getSingleCoffee} from '../store
