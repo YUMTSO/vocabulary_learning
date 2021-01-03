1310c374a757f87bcf8bdd2ec6e5e7eb269211f1
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2021-01-03 05:44:25 +0000

    zram: Disable allocation failure logging

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -11,3 +11,66 @@ const Navbarfunc = ({handleClick, isLoggedIn}) => (
   <div>
     {isLoggedIn ? (
       <div>
+        <Navbar bg="light" expand="lg" sticky="top">
+          <Navbar.Brand href="/home">Expresso.js</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          {/* The navbar will show these links after you log in */}
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Link className="navbar" to="/">
+                Home
+              </Link>
+              <Link className="navbar" to="/about-us">
+                About Us
+              </Link>
+              <Link className="navbar" to="/all-coffee">
+                Our Coffee
+              </Link>
+              <Link className="navbar" to="/cart">
+                Cart
+              </Link>
+
+              <Link className="navbar" to="/" onClick={handleClick}>
+                {' '}
+                Log Out{' '}
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    ) : (
+      <div>
+        <Navbar bg="light" expand="lg" sticky="top">
+          <Navbar.Brand href="/" className="banner">
+            Expresso.js
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Link className="navbar" to="/" onClick={handleClick}>
+                {' '}
+                Home{' '}
+              </Link>
+              <Link className="navbar" to="/about-us">
+                About Us
+              </Link>
+              <Link className="navbar" to="/allcoffee">
+                Our Coffee
+              </Link>
+              <Link className="navbar" to="/cart">
+                Cart
+              </Link>
+
+              <Link className="navbar" to="/sign-in">
+                Sign In
+              </Link>
+              <Link className="navbar" to="/signup">
+                Sign Up
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    )}
+  </div>
+)
