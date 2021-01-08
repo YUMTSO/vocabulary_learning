fdbf46e3658ee7e577ea9c70b940842dac5f4143
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2021-01-08 15:46:18 +0000

    make tests pass

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -2,4 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import AllCoffees from './AllCoffees'
-import {Link} from 'react-router-dom
+import {Link} from 'react-router-dom'
+
+/**
+ * COMPONENT
+ */
+export const UserHome = props =
