561e213155e5e5e1fcd236f5bf85d2797124d94c
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2021-01-26 19:24:07 +0000

    Update mip-story.js

--- client/routes.js
+++ client/routes.js
@@ -1,3 +1,5 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {withRouter, Route, Swit
+import {withRouter, Route, Switch} from 'react-router-dom'
+import PropTypes from 'prop-types'
+import {Login, Si
