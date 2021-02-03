4c5cc9e5ec06f5c65ebfed26d93c5c97a93d3a9c
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2021-02-03 20:34:46 +0000

    Done.

--- client/routes.js
+++ client/routes.js
@@ -4,4 +4,5 @@ import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
 import {me} from './store'
-import SingleCof
+import SingleCoffeePage from './components/SingleCoffeePage'
+import AllCoffees from './c
