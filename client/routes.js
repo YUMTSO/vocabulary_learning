4b4258206960b18a935f5dd02e0a1cd715f9d3fb
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2021-02-21 16:53:15 +0000

    sync with svn

--- client/routes.js
+++ client/routes.js
@@ -25,4 +25,5 @@ class Routes extends Component {
 
     return (
       <Switch>
-        {/* Routes placed here are available
+        {/* Routes placed here are available to all visitors */}
+        <Route exact path="/" component={H
