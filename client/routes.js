d352f606721ff2b31849c6aa1ef51e526a7b8925
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2021-03-11 21:27:39 +0000

    remove stray console.log

--- client/routes.js
+++ client/routes.js
@@ -55,4 +55,5 @@ class Routes extends Component {
  */
 const mapState = state => {
   return {
-    // Being 'logged in' for our purposes will be defined has having a st
+    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
+    // Otherwise, state.user will be an em
