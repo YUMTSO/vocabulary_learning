6b43c05f1aea73925e5e0a5b13c19341cca263c0
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2021-05-03 20:32:27 +0000

    Cleanup makefile

--- client/store/user.js
+++ client/store/user.js
@@ -43,3 +43,30 @@ export const auth = (email, password, method) => async dispatch => {
     dispatch(getUser(res.data))
     history.push('/home')
   } catch (dispatchOrHistoryErr) {
+    console.error(dispatchOrHistoryErr)
+  }
+}
+
+export const logout = () => async dispatch => {
+  try {
+    await axios.post('/auth/logout')
+    dispatch(removeUser())
+    history.push('/login')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultUser, action) {
+  switch (action.type) {
+    case GET_USER:
+      return action.user
+    case REMOVE_USER:
+      return defaultUser
+    default:
+      return state
+  }
+}
