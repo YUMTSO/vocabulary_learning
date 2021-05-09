4a087f3908eea284f95e6fb8c4d2a92443cf0ab7
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2021-05-09 07:36:41 +0000

    mah

--- client/store/user.spec.js
+++ client/store/user.spec.js
@@ -3,4 +3,6 @@
 import {expect} from 'chai'
 import {me, logout} from './user'
 import axios from 'axios'
-import MockAdapter from 'axios-mock-adapter
+import MockAdapter from 'axios-mock-adapter'
+import configureMockStore from 'redux-mock-store'
+import thunkMiddl
