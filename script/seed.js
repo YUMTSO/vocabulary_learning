10a959dcdc23afdd89bf28afa2f8e9aea7ee7e0b
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2021-11-06 19:46:52 +0000

    Update Examples.md

--- script/seed.js
+++ script/seed.js
@@ -94,3 +94,61 @@ const seedProducts = [
     price: 14.5,
     imgUrl:
       'http://www.saltpgh.com/wp-content/uploads/2016/11/Dark-Roast-coffee-bean.jpg'
+  },
+  {
+    name: 'Juggarnaut Espresso',
+    description: 'mauris viverra diam vitae quam suspendisse potenti nullam',
+    preparation: 'Teal',
+    origin: 'Russia',
+    weight: 4.6,
+    quantity: 12,
+    price: 82.48,
+    imgUrl:
+      'https://www.freshcup.com/wp-content/uploads/2017/04/PercJuggernaut.jpg'
+  },
+  {
+    name: 'Yirgacheffe',
+    description: 'pede justo eu massa donec dapibus',
+    preparation: 'Crimson',
+    origin: 'Philippines',
+    weight: 4.3,
+    quantity: 99,
+    price: 33.13,
+    imgUrl: 'http://www.macchomeless.org/wp-content/uploads/2018/03/coffee.jpg'
+  },
+  {
+    name: 'Pate - Peppercorn',
+    description: 'nisl duis ac nibh fusce lacus purus',
+    preparation: 'Yellow',
+    origin: 'Dominican Republic',
+    weight: 5.3,
+    quantity: 70,
+    price: 29.4,
+    imgUrl:
+      'https://vrteje8p14-flywheel.netdna-ssl.com/wp-content/uploads/2017/10/subscriptions-1824_2x.jpg'
+  },
+  {
+    name: 'Sumatra Musang',
+    description: 'in consequat ut nulla sed accumsan felis ut at dolor',
+    preparation: 'Aquamarine',
+    origin: 'Indonesia',
+    weight: 3.2,
+    quantity: 74,
+    price: 34.49,
+    imgUrl:
+      'https://5.imimg.com/data5/WQ/JP/MY-10901605/coffee-packaging-bags-500x500.jpg'
+  },
+  {
+    name: 'Kilimanjaro',
+    description:
+      'cras non velit nec nisi vulputate nonummy maecenas tincidunt lacus',
+    preparation: 'Khaki',
+    origin: 'Brazil',
+    weight: 6.1,
+    quantity: 59,
+    price: 65.81,
+    imgUrl:
+      'https://www.bagsandpouches.hk/wp-content/uploads/2016/10/Coffee-Packaging-7.jpg'
+  },
+  {
+    name: 'Cheese - Le Cheve Noir',
