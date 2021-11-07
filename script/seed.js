17bb8c8d304c6862d360e267d471f9ecd3d64964
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2021-11-07 09:22:49 +0000

    Upgrade bundler gem

--- script/seed.js
+++ script/seed.js
@@ -210,3 +210,72 @@ const seedUsers = [
 const seedOrders = [
   {
     userId: 1,
+    completed: true
+  }
+]
+
+const seedOrdersProducts = [
+  {
+    orderId: 1,
+    productId: 1,
+    quantity: 10
+  }
+]
+
+async function seed() {
+  await db.sync({force: true})
+  console.log('db synced!')
+
+  await Promise.all(
+    seedUsers.map(user => {
+      return User.create(user, {include: [Addresses]})
+    })
+  )
+
+  await Promise.all(
+    seedProducts.map(product => {
+      return Products.create(product)
+    })
+  )
+
+  await Promise.all(
+    seedOrders.map(orders => {
+      return Orders.create(orders)
+    })
+  )
+
+  await Promise.all(
+    seedOrdersProducts.map(orderProducts => {
+      return OrdersProducts.create(orderProducts)
+    })
+  )
+
+  console.log(`seeded successfully`)
+}
+
+// We've separated the `seed` function from the `runSeed` function.
+// This way we can isolate the error handling and exit trapping.
+// The `seed` function is concerned only with modifying the database.
+async function runSeed() {
+  console.log('seeding...')
+  try {
+    await seed()
+  } catch (err) {
+    console.error(err)
+    process.exitCode = 1
+  } finally {
+    console.log('closing db connection')
+    await db.close()
+    console.log('db connection closed')
+  }
+}
+
+// Execute the `seed` function, IF we ran this module directly (`node seed`).
+// `Async` functions always return a promise, so we can use `catch` to handle
+// any errors that might occur inside of `seed`.
+if (module === require.main) {
+  runSeed()
+}
+
+// we export the seed function for testing purposes (see `./seed.spec.js`)
+module.exports = seed
