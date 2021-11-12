b4b313929db9b4ee8390332f0bfcdd0e92e59d25
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2021-11-12 21:26:04 +0000

    Regenerated gemspec for version 0.1.0

--- server/api/orders.js
+++ server/api/orders.js
@@ -3,4 +3,11 @@ const {Orders, OrdersProducts, User, Products} = require('../db/models')
 
 router.get('/cart', async (req, res, next) => {
   try {
-    const orders = await Orders.findAll({include: [{model: Products}
+    const orders = await Orders.findAll({include: [{model: Products}]})
+    res.json(orders)
+  } catch (err) {
+    next(err)
+  }
+})
+
+///
