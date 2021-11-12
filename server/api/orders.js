b65328dc176497aa2e7576345ea4dcdbaeced4ae
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2021-11-12 17:41:44 +0000

    docs(readme)

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,2 +1,5 @@
 const router = require('express').Router()
-const {Orders, OrdersProducts, User, Products} = require('../d
+const {Orders, OrdersProducts, User, Products} = require('../db/models')
+
+router.get('/cart', async (req, res, next) => {
+  tr
