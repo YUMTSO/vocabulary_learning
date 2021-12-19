abe468cfb7eb38faafe24edb53851318f2b5f000
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2021-12-19 07:50:01 +0000

    fix: tidy php version reqs

--- server/api/products.js
+++ server/api/products.js
@@ -23,4 +23,9 @@ router.post('/', async (req, res, next) => {
   try {
     const newCoffee = await Products.create(req.body)
     res.status(200).json(newCoffee)
-  } catch (e
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:id', async (req, res, next) =
