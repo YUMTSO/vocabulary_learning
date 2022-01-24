0a410cadb28cb925e155062757968318e9c02c3f
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-01-24 12:51:26 +0000

    cifs: reinstate the forcegid option

--- server/auth/index.js
+++ server/auth/index.js
@@ -2,4 +2,6 @@ const router = require('express').Router()
 const User = require('../db/models/user')
 module.exports = router
 
-router.post('/l
+router.post('/login', async (req, res, next) => {
+  try {
+    const user = await User.findOne
