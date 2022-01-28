b43f029b8ef3a0f54a442880df15566475daf08c
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-01-28 12:32:44 +0000

    ARM: plat-orion: Fix num_resources and id for ge10 and ge11

--- server/auth/index.js
+++ server/auth/index.js
@@ -8,4 +8,5 @@ router.post('/login', async (req, res, next) => {
     if (!user) {
       console.log('No such user found:', req.body.email)
       res.status(401).send('Wrong username and/or password')
-    } else if (!use
+    } else if (!user.correctPassword(req.body.password)) {
+      console.log('Incorrect password for 
