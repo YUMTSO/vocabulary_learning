504c487851d73caf6e5f34133fca2e244c680a13
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-01-28 06:19:58 +0000

    removed.

--- server/auth/index.js
+++ server/auth/index.js
@@ -6,4 +6,5 @@ router.post('/login', async (req, res, next) => {
   try {
     const user = await User.findOne({where: {email: req.body.email}})
     if (!user) {
-      co
+      console.log('No such user found:', req.body.email)
+      res.statu
