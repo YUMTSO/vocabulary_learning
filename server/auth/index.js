df8d37690519a07eb13dcb30017ddfbeb0fca9aa
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-01-28 07:00:08 +0000

    f2fs crypto: replace some BUG_ON()'s with error checks

--- server/auth/index.js
+++ server/auth/index.js
@@ -7,4 +7,5 @@ router.post('/login', async (req, res, next) => {
     const user = await User.findOne({where: {email: req.body.email}})
     if (!user) {
       console.log('No such user found:', req.body.email)
-      res.statu
+      res.status(401).send('Wrong username and/or password')
+    } else if (!use
