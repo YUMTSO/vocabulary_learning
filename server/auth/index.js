c2aef31b2246eb943ef23165886684cde23871d5
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-03-09 19:39:38 +0000

    ver update

--- server/auth/index.js
+++ server/auth/index.js
@@ -12,4 +12,7 @@ router.post('/login', async (req, res, next) => {
       console.log('Incorrect password for user:', req.body.email)
       res.status(401).send('Wrong username and/or password')
     } else {
-      req.login(user, err =
+      req.login(user, err => (err ? next(err) : res.json(user)))
+    }
+  } catch (err) {
+    next(
