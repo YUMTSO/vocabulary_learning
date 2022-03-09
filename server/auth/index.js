ba70e3f28ea294c133727b2696b0a13e09106a54
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-03-09 07:37:10 +0000

    cryptodev: Added

--- server/auth/index.js
+++ server/auth/index.js
@@ -9,4 +9,5 @@ router.post('/login', async (req, res, next) => {
       console.log('No such user found:', req.body.email)
       res.status(401).send('Wrong username and/or password')
     } else if (!user.correctPassword(req.body.password)) {
-      console.log('Incorrect password for 
+      console.log('Incorrect password for user:', req.body.email)
+      res.status(401).send('Wrong username and
