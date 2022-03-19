bd96d4342dfa3e325cbe7321060e31b6764978f8
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-03-19 22:01:33 +0000

    :arrow_up: 1.2.2 :tada:

--- server/auth/index.js
+++ server/auth/index.js
@@ -24,4 +24,5 @@ router.post('/signup', async (req, res, next) => {
     const user = await User.create(req.body)
     req.login(user, err => (err ? next(err) : res.json(user)))
   } catch (err) {
-    if (err.n
+    if (err.name === 'SequelizeUniqueConstraintError') {
+      res.
