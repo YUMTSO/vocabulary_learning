d197be86e228ad8996f489b2d4e275061c3b9cd0
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-05-04 18:46:09 +0000

    Translated using Weblate (Belarusian)

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
-const database
+const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+
+const db = new S
