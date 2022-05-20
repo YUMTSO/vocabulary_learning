ba539cfcf72c665856ebe673b0eb65e919d0b93e
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-05-20 05:41:25 +0000

    Rename package

--- server/db/db.js
+++ server/db/db.js
@@ -14,4 +14,5 @@ module.exports = db
 // This is a global Mocha hook used for resource cleanup.
 // Otherwise, Mocha v4+ does not exit after tests.
 if (process.env.NODE_ENV === 'test') {
-  afte
+  after('close database connection', () => db.close())
+}
