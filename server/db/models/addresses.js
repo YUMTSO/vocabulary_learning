001e075cb3f9e1cd451edd1dd9c9b257077fd8fa
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-06-17 19:15:20 +0000

    Fix minor bug

--- server/db/models/addresses.js
+++ server/db/models/addresses.js
@@ -2,4 +2,7 @@ const crypto = require('crypto')
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Addresses = db.define(
+const Addresses = db.define('addresses', {
+  street: {
+    type: Sequelize.STRING,
+    uniqu
