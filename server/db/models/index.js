4d812f8529976f1afbcc532cb43c3fd412794e41
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-08-04 05:35:12 +0000

    Fixing more tests.

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -7,4 +7,9 @@ const db = require('../db')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost
+ * ex. if we had another model called BlogPost, we might say:
+ *
+ *    BlogPost.belongsTo(User)
+ */
+
+Addresses.belongsTo(User
