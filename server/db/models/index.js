80cc79b1d93afe1470c30e74d5b038f1aae8ce7b
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-08-15 19:05:21 +0000

    bower dependencies

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -29,4 +29,6 @@ Products.belongsToMany(Orders, {through: 'OrdersProducts'})
 
 /**
  * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/mode
+ * we can just require it from 'db/models'
+ * for example, we can say: const {User} = require('../db/models')
+ * inste
