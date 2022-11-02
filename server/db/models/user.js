b3d3a4befa27cb2eacc6fe03b1163deb5f5d515f
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-11-02 10:46:57 +0000

    fix release script

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,3 +3,8 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
+  first_name: {
+    type: Sequelize.STRING,
+    allowNull: true,
+    validate: {
+    
