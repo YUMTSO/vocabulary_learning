0066671b30ae716fc70589861202105c45bdac23
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-11-16 12:32:07 +0000

    add some codes

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -32,4 +32,12 @@ const User = db.define('user', {
     // Making `.salt` act like a function hides it when serializing to JSON.
     // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
-      return () => this.getDataValue('salt
+      return () => this.getDataValue('salt')
+    }
+  },
+  googleId: {
+    type: Sequelize.STRING
+  }
+})
+
+m
