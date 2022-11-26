ddbf3d6b01aa86c54191821e1301565bf61e63f0
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-11-26 13:09:26 +0000

    Restore tests

--- server/index.js
+++ server/index.js
@@ -3,4 +3,5 @@ const express = require('express')
 const morgan = require('morgan')
 const compression = require('compression')
 const session = require('express-session')
-const passport = require(
+const passport = require('passport')
+const SequelizeStore = require('connect-session-sequeliz
