e86d9ba82ad7637e9f928978adf9231c4784f4aa
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-12-14 15:04:44 +0000

    Updated the comments

--- server/index.js
+++ server/index.js
@@ -22,4 +22,5 @@ if (process.env.NODE_ENV === 'test') {
  * In your development environment, you can keep all of your
  * app's secret API keys in a file called `secrets.js`, in your project
  * root. This file is included in the .gitignore - it will NOT be tracked
- * or show up on Github. On your production server,
+ * or show up on Github. On your production server, you can add these
+ * keys as environment variables, so that they 
