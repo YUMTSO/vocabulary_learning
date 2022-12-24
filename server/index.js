980971da9b969bfca29c512f85ae87065920eec8
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-12-24 12:24:37 +0000

    Updated MAINTAINERS file [CI SKIP]

--- server/index.js
+++ server/index.js
@@ -23,4 +23,7 @@ if (process.env.NODE_ENV === 'test') {
  * app's secret API keys in a file called `secrets.js`, in your project
  * root. This file is included in the .gitignore - it will NOT be tracked
  * or show up on Github. On your production server, you can add these
- * keys as environment variables, so that they 
+ * keys as environment variables, so that they can still be read by the
+ * Node process on process.env
+ */
+if
