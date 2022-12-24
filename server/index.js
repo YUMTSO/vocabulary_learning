aeb54d35da2ddf08e573c06a96a5c76dfa33a005
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2022-12-24 21:33:13 +0000

    Fix parsing

--- server/index.js
+++ server/index.js
@@ -29,4 +29,7 @@ if (process.env.NODE_ENV === 'test') {
 if (process.env.NODE_ENV !== 'production') require('../secrets')
 
 // passport registration
-passport.serializeUser((user, done) => done(n
+passport.serializeUser((user, done) => done(null, user.id))
+
+passport.deserializeUser(async (id, done) => {
+  t
