3e9c245f66c3a1f0d39c29665492b4688160f4ab
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2023-02-27 11:01:34 +0000

    start new

--- server/index.js
+++ server/index.js
@@ -114,4 +114,5 @@ async function bootApp() {
   await startListening()
 }
 // This evaluates as true when this file is run directly from the command line,
-// i.e. when we say 'node server/index.js' (or 'nodemon server/i
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this mo
