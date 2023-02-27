b474979ce615a719b52ee05632833a76d5af2ac8
YUMTSO <111027089+YUMTSO@users.noreply.github.com>
2023-02-27 21:13:39 +0000

    Satisfy rubocop

--- server/index.js
+++ server/index.js
@@ -115,4 +115,5 @@ async function bootApp() {
 }
 // This evaluates as true when this file is run directly from the command line,
 // i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
-// It will evaluate false when this mo
+// It will evaluate false when this module is required by another module - for example,
+/
